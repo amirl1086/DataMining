62f9f5d610989cb75fc18f2acc9e4f6e8873ed84
Amir_Lavi <amirl1086@gmail.com>
2016-12-19 23:13:02 +0200

    added functionality for reading signatures and creating pkl file

--- ex1.py
+++ ex1.py
@@ -1,19 +1,43 @@
 
 import pandas as pd
+import pickle
 
 
-dictionary = []
+dictionary = {}
+
+
+def create_signature(word_list):
+    word_dic = {}
+    for word in word_list:
+        if word not in word_dic:
+            word_dic[word] = 0
+        else:
+            word_dic[word] += 1
+    return word_dic
 
 
 def init():
-    train_csv = pd.read_csv("./Train.csv")
+    train_csv = pd.read_csv("./Test.csv")
+    test_pkl = open('test.pkl', 'wb')
+
     global dictionary
 
-    for description_data in train_csv['FullDescription']:
+    i = 0
+    for description_data in train_csv["FullDescription"]:
+
+        # create the list of words
         description_row = list(set(description_data.split(" ")))
-        temp = list(set(description_row) - set(dictionary))
-        dictionary.append(temp)
+
+        # insert to dictionary
+        dictionary["row_" + str(i)] = create_signature(description_row)
+
+        # insert to pkl file
+        pickle.dump(dictionary["row_" + str(i)], test_pkl)
+
+        for data in dictionary["row_" + str(i)]:
+            print(str(i) + data)
+
+        i += 1
 
 
 init()
-print(dictionary)
