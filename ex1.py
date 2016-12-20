b05dcdd3e435aba44e01645475e9badd007f67ce
Amir_Lavi <amirl1086@gmail.com>
2016-12-20 22:28:44 +0200

    continued transform document to shingles

--- ex1.py
+++ ex1.py
@@ -1,22 +1,60 @@
 
 import pandas as pd
 import pickle
+import time
+import numpy as np
+import re
+import hashlib
+
+
+# from simhash import Simhash
+from collections import deque
+from nltk import ngrams
 
 
 dictionary = {}
 
 
-def create_signature(word_list):
-    word_dic = {}
-    for word in word_list:
-        if word not in word_dic:
-            word_dic[word] = 0
+def create_signature(doc):
+    # remove unnecessary characters
+    doc = re.compile('[^a-zA-Z ]').sub('', doc)
+    doc = doc.lower()
+
+    # create bin hash
+    six_grams = ngrams(doc.split(), 3)
+    doc_shingles = []
+
+    for grams in six_grams:
+        hash_shingle = (''.join(grams)).encode('utf-8')
+        # hash_shingle = int(hashlib.md5(shingle.encode('utf-8')).hexdigest(), 16)
+        doc_shingles.append(bin(int(hashlib.md5(hash_shingle.encode('utf-8')).hexdigest(), 16))[2:34])
+
+    return create_sig_from_shingels(doc_shingles)
+
+
+def create_sig_from_shingels(doc_shingles):
+    sig_hash = [0] * 32  # hash size
+
+    list_of_bin = []
+    for i in range(len(doc_shingles)):
+        list_of_bin.append(np.fromstring(doc_shingles[i], 'u1') - ord('0'))
+
+    a = np.matrix(list_of_bin)
+
+    sum_of_the_matrix = a.sum(axis=0)
+
+    for i in range(len(sig_hash)):
+        if sum_of_the_matrix[0, i] < len(doc_shingles) / 2:
+            sig_hash[i] = 1
         else:
-            word_dic[word] += 1
-    return word_dic
+            sig_hash[i] = 0
+
+    return sig_hash
 
 
 def init():
+    delta = time.time()
+
     train_csv = pd.read_csv("./Test.csv")
     test_pkl = open('test.pkl', 'wb')
 
@@ -25,11 +63,8 @@ def init():
     i = 0
     for description_data in train_csv["FullDescription"]:
 
-        # create the list of words
-        description_row = list(set(description_data.split(" ")))
-
         # insert to dictionary
-        dictionary["row_" + str(i)] = create_signature(description_row)
+        dictionary["row_" + str(i)] = create_signature(description_data)
 
         # insert to pkl file
         pickle.dump(dictionary["row_" + str(i)], test_pkl)
